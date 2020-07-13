bd371cb6e4346477b875496c61ec249a6a5d51b5
DevilStars <mr.devilstar@gmail.com>
2020-07-13 12:10:38 +0300

    check abilities method

--- mixins/userMixin.js
+++ mixins/userMixin.js
@@ -4,9 +4,20 @@ export default {
   computed: {
     ...mapGetters({
       token: 'modules/user/auth/getToken',
+      abilities: 'modules/user/auth/getAbilities'
     }),
+
     checkAuth() {
       return !!this.token;
+    },
+  },
+  methods: {
+    checkAbilities(abilities) {
+      let rule = (e, index, array) => {
+        return this.abilities.includes(e);
+      };
+
+      return abilities.every(rule);
     }
   }
 }
