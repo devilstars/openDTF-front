7e907245e7af6851abca8dab94de50dbfc6e4d40
DevilStars <mr.devilstar@gmail.com>
2020-09-17 15:41:58 +0300

    dependency update.

--- pages/admin/users.vue
+++ pages/admin/users.vue
@@ -5,12 +5,31 @@
 </template>
 
 <script>
-    export default {
-        name: "users",
-        head: {
-          title: "[Dashboard] Пользователи"
-        }
-    }
+import Vue from "vue";
+
+export default {
+  name: "users",
+  head: {
+    title: "[Dashboard] Пользователи"
+  },
+  // async asyncData({ params, $axios }) {
+  //   return await $axios.post(Vue.prototype.$config.dashboardUrls.usersUrl.list)
+  //     .then(response => {
+  //       return {list: response.data};
+  //     }).catch(error => {
+  //       if (error.response.status === 422) {
+  //         // commit('setErrors', error.response.data.errors)
+  //         this.$toast.error('Проверьте введённые данные')
+  //       }
+  //       if (error.response.status === 404) {
+  //         this.$toast.error(error.response.data.message)
+  //       }
+  //     });
+  // }
+  // async fetch({store, params}) {
+  //   await store.dispatch('modules/admin/users/getUsers');
+  // },
+}
 </script>
 
 <style scoped>
