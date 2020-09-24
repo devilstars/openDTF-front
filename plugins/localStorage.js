076c3cd9833f5d4a0f849d3c30dd1f5796615388
DevilStars <mr.devilstar@gmail.com>
2020-09-24 10:40:01 +0300

    local storage is now protected with RC4 and compressed

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -1,8 +1,15 @@
 import createPersistedState from 'vuex-persistedstate'
+import SecureLS from "secure-ls";
+let ls = new SecureLS({ encodingType: 'rc4', isCompression: true, encryptionSecret: 'Zj8,;ZN?M><F7dL+' });
 
 export default ({store}) => {
   window.onNuxtReady(() => {
     createPersistedState({
+      storage: {
+        getItem: (key) => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: (key) => ls.remove(key),
+      },
       paths: [
         'modules.user'
       ]
